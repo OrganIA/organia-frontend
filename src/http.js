d9593eb74a297bf304330adea4d200d18d7a41b7
OkilSaber <okil-saber.lakhdari@epitech.eu>
2021-05-28 11:17:07 +0200

    Add use of .env file for the backend baseurl

--- src/http.js
+++ src/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://organia.francecentral.cloudapp.azure.com:8000/api/v1/",
+    baseURL: process.env.API_URL,
     mode: 'no-cors',
     headers: {
         "Content-Type": "application/json"
