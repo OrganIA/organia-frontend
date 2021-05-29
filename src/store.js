500c97495917af3a77faee5d9bb48292c27c6a34
OkilSaber <okil-saber.lakhdari@epitech.eu>
2021-05-29 16:43:07 +0200

    Add authorizatio token in the store

--- src/store.js
+++ src/store.js
@@ -3,11 +3,18 @@ import { createStore } from "vuex"
 const store = createStore({
     state() {
         return {
+            token: "",
         }
     },
     mutations: {
+        updateToken(state, newToken) {
+            state.token = newToken;
+        }
     },
     getters: {
+        getToken(state) {
+            return state.token;
+        }
     }
 })
 
