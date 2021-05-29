87dcfd6414d8dbaed158a2f96cda888fc99af8a4
OkilSaber <okil-saber.lakhdari@epitech.eu>
2021-05-29 16:43:41 +0200

    Add api call to update the user

--- src/components/EditPanel.vue
+++ src/components/EditPanel.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
-    <h1>WALOUUUU</h1>
-    <p>{{ id }}</p>
+      <a @click="backToParent()">Back</a>
+    <form class="form-fields" @submit.prevent="submitForm()">
+      <input class="form-input" type="text" v-model="user.name" />
+      <input class="form-input" type="email" v-model="user.email" />
+      <input class="form-input" type="password" v-model="user.password" />
+      <button type="submit">Save Changes</button>
+    </form>
   </div>
 </template>
 
@@ -12,6 +17,7 @@ export default {
   name: "EditPanel",
   props: {
     id: String,
+    parent: String,
   },
   data() {
     return {
@@ -23,12 +29,28 @@ export default {
       http
         .get(`/users/${this.id}`)
         .then((response) => {
-          this.user = response;
+          this.user = response.data;
         })
         .catch((error) => {
           console.log(error);
         });
     },
+    submitForm() {
+        console.log(this.$store.getters.getToken)
+        http.post(`/users/${this.id}`, {
+            headers: {
+                autorization: `Bearer ${this.$store.getters.getToken}`
+            },
+            params: {
+                name: this.user.name,
+                email: this.user.email,
+                password: this.user.password,
+            }
+        })
+    },
+    backToParent() {
+        this.$parent.$data.CurrentPanel = this.parent;
+    }
   },
   created() {
     this.getUserByID();
@@ -37,4 +59,75 @@ export default {
 </script>
 
 <style>
+a,
+button {
+  cursor: pointer;
+}
+
+a,
+a:visited,
+a:active {
+  color: var(--blue);
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+
+button:hover,
+.button:hover {
+  text-decoration: none;
+}
+
+input[type="number"] {
+  min-width: 4em;
+}
+
+button.active,
+.button.active,
+[type="submit"] {
+  background-color: var(--blue);
+  color: white;
+}
+
+.form-fields {
+  display: flex;
+  flex-flow: wrap row;
+  justify-content: space-between;
+}
+
+.form-input {
+  display: flex;
+  flex-direction: column;
+  margin: 1em 0;
+  width: 100%;
+}
+
+.form-input.small {
+  width: calc(50% - 1em);
+}
+
+.form-input.mini {
+  width: calc(33% - 1em);
+}
+
+.form-error {
+  font-size: small;
+  color: var(--red);
+}
+
+.row {
+  display: flex;
+  flex-direction: row;
+}
+
+.column {
+  display: flex;
+  flex-direction: column;
+}
+
+.hide {
+  display: none;
+}
 </style>
