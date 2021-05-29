8f0ba1e36869d4a0d5e616c0564261df2f1cbd75
OkilSaber <okil-saber.lakhdari@epitech.eu>
2021-05-30 00:15:17 +0200

    Update imports

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import App from './pages/App.vue'
+import App from './App.vue'
 import store from './store'
 import router from './router'
 import cookies from 'vue3-cookies'
