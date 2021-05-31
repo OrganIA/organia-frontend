e4f4a62d33efda7cf1003988e3d690cce6d7808f
OkilSaber <okil-saber.lakhdari@epitech.eu>
2021-05-31 22:00:04 +0200

    Delete useless rule

--- src/http.js
+++ src/http.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 export default axios.create({
     baseURL: process.env.VUE_APP_API_URL,
-    mode: 'no-cors',
     headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
     },
 });
