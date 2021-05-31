b9f34db48d437ceda56e543c63bc40d36c461512
OkilSaber <okil-saber.lakhdari@epitech.eu>
2021-05-31 14:08:17 +0200

    Add user variables in the store as well as corresponding functions

--- src/store.js
+++ src/store.js
@@ -4,17 +4,36 @@ const store = createStore({
     state() {
         return {
             token: "",
+            name: "",
+            email: "",
         }
     },
     mutations: {
         updateToken(state, newToken) {
             state.token = newToken;
-        }
+        },
+        updateName(state, newName) {
+            state.name = newName;
+        },
+        updateEmail(state, newEmail) {
+            state.email = newEmail;
+        },
+        login(state, newEmail, newName, newToken) {
+            state.token = newToken;
+            state.name = newName;
+            state.email = newEmail;
+        },
     },
     getters: {
         getToken(state) {
             return state.token;
-        }
+        },
+        getName(state) {
+            return state.name;
+        },
+        getEmail(state) {
+            return state.email;
+        },
     }
 })
 
