5ae5ce1761ac5105add62b89c512b27ab963b18d
Victor_Hamon <victor.hamon@epitech.eu>
2021-06-09 09:45:05 +0200

    [MOD] sidebar buttons text decorations removed and hover feedback added

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -12,11 +12,11 @@
     </header>
     <div id="navlinks">
       <div id="navbox">
-        <router-link to="/receivers" v-bind:class="{ active: isActive }" v-on:click="ReceiverClicked">
+        <router-link class="sidebar-link" to="/receivers" v-bind:class="{ active: isActive }" v-on:click="ReceiverClicked">
           <i class="fa fa-address-card"></i>
           <span class="nav-text">Receveurs</span>
         </router-link>
-        <router-link to="/donors" v-bind:class="{ active: !isActive }" v-on:click="DonorClicked">
+        <router-link class="sidebar-link" to="/donors" v-bind:class="{ active: !isActive }" v-on:click="DonorClicked">
           <i class="fa fa-address-card" aria-hidden="true"></i>
           <span class="nav-text">Donneurs</span>
         </router-link>
