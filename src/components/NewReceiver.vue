17f749ff4eef0a755e1c9bbc12ebf0794f7c62f7
OkilSaber <okil-saber.lakhdari@epitech.eu>
2021-06-13 17:50:26 +0200

    Change api calls from persons to listings receivers

--- src/components/NewReceiver.vue
+++ src/components/NewReceiver.vue
@@ -1,14 +1,50 @@
 <template>
   <div>
     <form @submit.prevent="createPerson()">
-      <input v-model="first_name" placeholder="first_name" type="text" />
-      <input v-model="last_name" placeholder="last_name" type="text" />
-      <input v-model="birthday" placeholder="birthday" type="date" />
-      <input v-model="description" placeholder="description" type="text" />
+      <input
+        v-model="first_name"
+        placeholder="first_name"
+        type="text"
+        required
+      />
+      <input v-model="last_name" placeholder="last_name" type="text" required />
+      <input v-model="birthday" placeholder="birthday" type="date" required />
+      <input
+        v-model="start_date"
+        placeholder="start date"
+        type="date"
+        required
+      />
+      <input
+        v-model="description"
+        placeholder="description"
+        type="text"
+        required
+      />
+      <input v-model="organ" placeholder="organ" type="text" required />
+      <select v-model="blood_type" name="abo" id="abo-select" required>
+        <option value="">--Please choose an option--</option>
+        <option value="A">A</option>
+        <option value="B">B</option>
+        <option value="O">O</option>
+        <option value="AB">AB</option>
+      </select>
+      <select v-model="rhesus" name="rhesus" id="rhesus-select" required>
+        <option value="">--Please choose an option--</option>
+        <option value="+">+</option>
+        <option value="-">-</option>
+      </select>
+      <select v-model="gender" name="gender" id="gender-select" required>
+        <option value="">--Please choose an option--</option>
+        <option value="MALE">MALE</option>
+        <option value="FEMALE">FEMALE</option>
+      </select>
+      <textarea v-model="notes" placeholder="notes" required />
       <input
         v-model="supervisor_id"
         placeholder="supervisor_id"
         type="number"
+        required
       />
       <button type="submit">S'inscrire</button>
     </form>
@@ -27,27 +63,45 @@ export default {
       birthday: "",
       description: "",
       supervisor_id: 0,
+      person_id: undefined,
+      start_date: "",
+      notes: "",
+      organ: "",
+      blood_type: "",
+      rhesus: "",
+      gender: "",
     };
   },
   created() {},
   methods: {
     createPerson() {
       http
-        .post(
-          "/persons",
-          {
-            first_name: this.first_name,
-            last_name: this.last_name,
-            birthday: this.birthday,
-            description: this.description,
-            supervisor_id: this.supervisor_id,
-          })
+        .post("/persons", {
+          first_name: this.first_name,
+          last_name: this.last_name,
+          birthday: this.birthday,
+          description: this.description,
+          supervisor_id: this.supervisor_id,
+          abo: this.blood_type,
+          rhesus: this.rhesus,
+          gender: this.gender,
+        })
         .then((response) => {
-          console.log(response);
+          this.person_id = response.data.id;
+          this.createReceiver();
         })
         .catch((error) => {
           console.log(error);
         });
+    },
+    createReceiver() {
+      http.post("/listings", {
+        start_date: this.start_date,
+        notes: this.notes,
+        organ: this.organ,
+        donor: false,
+        person_id: this.person_id,
+      });
     },
   },
 };
--- src/components/ReceiversPanel.vue
+++ src/components/ReceiversPanel.vue
@@ -23,13 +23,13 @@
         </thead>
         <tbody>
           <tr v-for="receiver in receivers" :key="receiver">
-            <td>{{ receiver.first_name }}</td>
-            <td>{{ receiver.last_name }}</td>
-            <td>{{ receiver.birthday }}</td>
-            <td>{{ receiver.gender }}</td>
-            <td>{{ receiver.blood_type }}</td>
+            <td>{{ receiver.person.first_name }}</td>
+            <td>{{ receiver.person.last_name }}</td>
+            <td>{{ receiver.person.birthday }}</td>
+            <td>{{ receiver.person.gender }}</td>
+            <td>{{ receiver.person.blood_type }}</td>
             <td>{{ receiver.organ }}</td>
-            <td>{{ receiver.created_at }}</td>
+            <td>{{ receiver.person.created_at }}</td>
             <td>
               <router-link :to="`/receivers/edit/${receiver.id}`">
                 <i class="fas fa-trash-alt"></i>
@@ -59,10 +59,11 @@ export default {
   methods: {
     getAllReceivers() {
       http
-        .get("/persons")
+        .get("/listings/receivers")
         .then((response) => {
+          console.log(response.data)
           response.data.forEach((element) => {
-            element.created_at = new Date(element.created_at).toDateString();
+            element.person.created_at = new Date(element.person.created_at).toDateString();
           });
           this.receivers = response.data;
         })
