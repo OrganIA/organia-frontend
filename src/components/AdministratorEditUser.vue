cdbfe05c80b2a1991135e6f48a93ae35dcf82744
OkilSaber <okil-saber.lakhdari@epitech.eu>
2021-06-13 20:05:17 +0200

    Remove useless inputs

--- src/components/AdministratorEditUser.vue
+++ src/components/AdministratorEditUser.vue
@@ -3,12 +3,7 @@
     <router-link class="btn back-button" to="/administrator">Back</router-link>
     <form class="form-editor">
       <div>
-        <input v-model="user.email" type="email" class="form-control">
-      </div>
-      <div class="horizontal-form-group">
-        <input v-model="user.created_at" type="date" class="form-control">
-        <input v-model="user.updated_at" type="date" class="form-control">
-
+        <input v-model="user.email" type="email" class="form-control" />
       </div>
       <button type="submit" class="btn btn-primary">Submit</button>
     </form>
@@ -31,27 +26,27 @@ export default {
   methods: {
     getUserByID() {
       http
-          .get(`/users/${this.id}`)
-          .then((response) => {
-            this.user = response.data;
-          })
-          .catch((error) => {
-            console.log(error);
-          });
+        .get(`/users/${this.id}`)
+        .then((response) => {
+          this.user = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
     submitForm() {
       http
-          .post({})
-          .then(() => {
-            this.$router.push("/administrator");
-          })
-          .catch((error) => {
-            console.log(error);
-          });
+        .post({})
+        .then(() => {
+          this.$router.push("/administrator");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
   },
   created() {
     this.getUserByID();
   },
-}
+};
 </script>
