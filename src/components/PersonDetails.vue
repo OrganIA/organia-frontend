5e4dce6e3d359a3859e64170b0fefcbd36b968d4
OkilSaber <okil-saber.lakhdari@epitech.eu>
2021-07-29 14:24:04 +0200

    Add informations display

--- src/components/PersonDetails.vue
+++ src/components/PersonDetails.vue
@@ -1,5 +1,30 @@
 <template>
-  <div></div>
+  <div>
+    <h2>Nom</h2>
+    <h4>{{ personLocal.person.last_name }}</h4>
+    <h2>Prénom</h2>
+    <h4>{{ personLocal.person.first_name }}</h4>
+    <h2>Date de naissance</h2>
+    <h4>{{ personLocal.person.birthday }}</h4>
+    <h2>Groupe Sanguin</h2>
+    <h4>{{ personLocal.person.blood_type }}</h4>
+    <h2>Genre</h2>
+    <h4>{{ personLocal.person.gender }}</h4>
+    <div v-if="personLocal.donor">
+      <h3>Demande un {{ personLocal.organ }}</h3>
+    </div>
+    <div v-else>
+      <h3>Donne un {{ personLocal.organ }}</h3>
+    </div>
+    <h2>Admis le:</h2>
+    <h4>{{ personLocal.start_date }}</h4>
+    <h2>Description:</h2>
+    <h4>{{ personLocal.person.description }}</h4>
+    <div v-if="personLocal.notes">
+      <h2>Notes:</h2>
+      <h4>{{ personLocal.notes }}</h4>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -17,7 +42,13 @@ export default {
   created() {
     // console.log(this.$router)
     this.personLocal = JSON.parse(this.person);
-    // console.log(this.personLocal);
+    console.log(this.personLocal);
   },
 };
 </script>
+
+<style scoped>
+h2, h3, h4 {
+  border: 10px, black;
+}
+</style>
