be5bff31a6ff7e10da880d51a4934c6050d728a4
OkilSaber <okil-saber.lakhdari@epitech.eu>
2021-08-11 14:22:45 +0200

    Fixed CSS style for the modal

--- src/components/PersonDetails.vue
+++ src/components/PersonDetails.vue
@@ -1,36 +1,34 @@
 <template>
-  <div>
-    <h2>Nom</h2>
-    <h4>{{ person.person.last_name }}</h4>
-    <h2>Prénom</h2>
-    <h4>{{ person.person.first_name }}</h4>
-    <h2>Date de naissance</h2>
-    <h4>{{ person.person.birthday }}</h4>
-    <h2>Groupe Sanguin</h2>
-    <h4>{{ person.person.blood_type }}</h4>
-    <h2>Genre</h2>
-    <h4>{{ person.person.gender }}</h4>
-    <div v-if="person.donor">
-      <h3>Demande un {{ person.organ }}</h3>
-      // console.log(receiver);
+    <div>
+      <h2>Nom</h2>
+      <h4>{{ person.person.last_name }}</h4>
+      <h2>Prénom</h2>
+      <h4>{{ person.person.first_name }}</h4>
+      <h2>Date de naissance</h2>
+      <h4>{{ person.person.birthday }}</h4>
+      <h2>Groupe Sanguin</h2>
+      <h4>{{ person.person.blood_type }}</h4>
+      <h2>Genre</h2>
+      <h4>{{ person.person.gender }}</h4>
+      <div v-if="!person.donor">
+        <h3>Demande un {{ person.organ }}</h3>
+      </div>
+      <div v-else>
+        <h3>Donne un {{ person.organ }}</h3>
+      </div>
+      <h2>Admis le:</h2>
+      <h4>{{ person.start_date }}</h4>
+      <h2>Description:</h2>
+      <h4>{{ person.person.description }}</h4>
+      <div v-if="person.notes">
+        <h2>Notes:</h2>
+        <h4>{{ person.notes }}</h4>
+      </div>
+      <button type="button" @click="closeModal">Fermer</button>
     </div>
-    <div v-else>
-      <h3>Donne un {{ person.organ }}</h3>
-    </div>
-    <h2>Admis le:</h2>
-    <h4>{{ person.start_date }}</h4>
-    <h2>Description:</h2>
-    <h4>{{ person.person.description }}</h4>
-    <div v-if="person.notes">
-      <h2>Notes:</h2>
-      <h4>{{ person.notes }}</h4>
-    </div>
-    <button type="button" @click="closeModal">Close</button>
-  </div>
 </template>
 
 <script>
-// import http from "../http";
 
 export default {
   name: "PersonDetails",
--- src/components/ReceiversPanel.vue
+++ src/components/ReceiversPanel.vue
@@ -1,55 +1,52 @@
 <template>
   <div id="main">
-    <main>
-      <div>
-        <h1 style="text-align: center">Liste d'attente</h1>
-        <p>
-          <router-link to="/receivers/add" class="button cypress-to-add">
-            Ajouter
-          </router-link>
-        </p>
-      </div>
+    <h1 style="text-align: center">Liste d'attente</h1>
+    <p>
+      <router-link to="/receivers/add" class="button cypress-to-add">
+        Ajouter
+      </router-link>
+    </p>
 
-      <table class="table-list">
-        <thead>
-          <tr>
-            <th>Prénom</th>
-            <th>Nom de famille</th>
-            <th>Date de naissance</th>
-            <th>Sexe</th>
-            <th>ABO</th>
-            <th>Organe</th>
-            <th>Arrivée</th>
-            <th>action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr
-            v-for="receiver in receivers"
-            :key="receiver"
-            @click="openModal(receiver)"
-          >
-            <td>{{ receiver.person.first_name }}</td>
-            <td>{{ receiver.person.last_name }}</td>
-            <td>{{ receiver.person.birthday }}</td>
-            <td>{{ receiver.person.gender }}</td>
-            <td>{{ receiver.person.blood_type }}</td>
-            <td>{{ receiver.organ }}</td>
-            <td>{{ receiver.person.created_at }}</td>
-            <td>
-              <router-link :to="`/receivers/edit/${receiver.person.id}`">
-                <i class="fas fa-edit"></i>
-              </router-link>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <person-details
-        v-if="showModal == true"
-        :person="currentReceiver"
-        @closeModal="closeModal"
-      />
-    </main>
+    <table class="table-list">
+      <thead>
+        <tr>
+          <th>Prénom</th>
+          <th>Nom de famille</th>
+          <th>Date de naissance</th>
+          <th>Sexe</th>
+          <th>ABO</th>
+          <th>Organe</th>
+          <th>Arrivée</th>
+          <th>action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="receiver in receivers"
+          :key="receiver"
+          @click="openModal(receiver)"
+        >
+          <td>{{ receiver.person.first_name }}</td>
+          <td>{{ receiver.person.last_name }}</td>
+          <td>{{ receiver.person.birthday }}</td>
+          <td>{{ receiver.person.gender }}</td>
+          <td>{{ receiver.person.blood_type }}</td>
+          <td>{{ receiver.organ }}</td>
+          <td>{{ receiver.person.created_at }}</td>
+          <td>
+            <router-link :to="`/receivers/edit/${receiver.person.id}`">
+              <i class="fas fa-edit"></i>
+            </router-link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <person-details
+      v-if="showModal == true"
+      :person="currentReceiver"
+      @closeModal="closeModal"
+      class="details"
+    />
   </div>
 </template>
 
@@ -90,11 +87,13 @@ export default {
       if (!this.showModal) {
         this.showModal = true;
         this.currentReceiver = receiver;
+        document.getElementById("bodiv").style.display = "initial";
       }
     },
     closeModal() {
       this.showModal = false;
       this.currentReceiver = {};
+      document.getElementById("bodiv").style.display = "none";
     },
   },
 };
