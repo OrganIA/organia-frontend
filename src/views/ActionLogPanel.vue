7318631f2217ae007ab174b8645a5fb30033c835
OkilSaber <okil-saber.lakhdari@epitech.eu>
2021-10-10 04:22:14 +0200

    Add start of action log panel

---
+++ src/views/ActionLogPanel.vue
@@ -0,0 +1,20 @@
+<template>
+    <p>Action Log</p>
+    <div v-for="action in actions" :key="action">
+        <p>action</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            actions: []
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
