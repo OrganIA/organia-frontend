a45807955566e709b7e5f031d91b335fc25643e1
VictorHamon <victor.hamon@epitech.eu>
2021-12-26 22:41:41 +0900

    [MOD] Logout now correctly redirects back to login page

---
+++ src/components/UserDropdown.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="dropdown avatar-dropdown is-hoverable">
+    <div class="dropdown-trigger ">
+      <button class="button dropdown-trigger" aria-haspopup="true" aria-controls="dropdown-menu3">
+        <span>Click me</span>
+        <span class="icon is-small">
+        <img src="https://synergy-it.gitlab.io/speekio/speekio-portal/assets/img/img_avatar.png"/>
+      </span>
+      </button>
+    </div>
+    <div class="dropdown-menu" id="dropdown-menu3" role="menu">
+      <div class="dropdown-content dropdown-correction">
+        <a href="#" class="dropdown-item">
+          Overview
+        </a>
+        <a href="#" class="dropdown-item">
+          Modifiers
+        </a>
+        <a href="#" class="dropdown-item">
+          Grid
+        </a>
+        <a href="#" class="dropdown-item">
+          Form
+        </a>
+        <a href="#" class="dropdown-item">
+          Elements
+        </a>
+        <a href="#" class="dropdown-item">
+          Components
+        </a>
+        <a href="#" class="dropdown-item">
+          Layout
+        </a>
+        <hr class="dropdown-divider">
+        <button
+            @click="logout"
+            class="block button is-danger is-light is-outlined"
+        >
+          Se déconnecter
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserDropdown",
+  emits: ["logout"],
+  methods: {
+    logout() {
+      this.$store.commit("logout");
+      this.$cookies.remove("token");
+      this.$emit("logout");
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
