fe245e050980a56148db06f4952f237dd65aed76
davidfarjonepitech <david.farjon@epitech.eu>
2022-02-14 22:01:21 -0800

    [ADD] Team Profile page at this url : ****/team, create cards we can fill but not finish

---
+++ src/views/TeamDescription.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="team-desc-page">
+    <div class="team-box">
+      <div class="team-color" style="background: #0b0b5e"></div>
+      <div class="team-image">W</div>
+      <div class="team-name">William WINQWIST</div>
+      <div class="team-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+    </div>
+
+    <div class="team-box">
+      <div class="team-color" style="background: #911671"></div>
+      <div class="team-image">D</div>
+      <div class="team-name">David FARJON</div>
+      <div class="team-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+    </div>
+
+    <div class="team-box">
+      <div class="team-color" style="background: #af1818"></div>
+      <div class="team-image">O</div>
+      <div class="team-name">Okil-Saber LAKHDARI</div>
+      <div class="team-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+    </div>
+
+    <div class="team-box">
+      <div class="team-color" style="background: #ff00c8"></div>
+      <div class="team-image">D</div>
+      <div class="team-name">Damien SAVATIER</div>
+      <div class="team-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+    </div>
+
+    <div class="team-box">
+      <div class="team-color" style="background: #00d1b2"></div>
+      <div class="team-image">F</div>
+      <div class="team-name">Felix BURDOT</div>
+      <div class="team-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+    </div>
+
+    <div class="team-box">
+      <div class="team-color" style="background: #00a1d1"></div>
+      <div class="team-image">G</div>
+      <div class="team-name">Guillaume BRAUX</div>
+      <div class="team-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+    </div>
+
+    <div class="team-box">
+      <div class="team-color" style="background: #00ff43"></div>
+      <div class="team-image">N</div>
+      <div class="team-name">Nicolas YAPOBI</div>
+      <div class="team-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+    </div>
+
+    <div class="team-box">
+      <div class="team-color" style="background: #ccdf32"></div>
+      <div class="team-image">V</div>
+      <div class="team-name">Victor HAMON</div>
+      <div class="team-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TeamDescription",
+};
+</script>
