f66ec1d3c879b822384fcd41a8af63e4b66e3eee
NicolasYapobi <you@example.com>
2022-03-10 12:53:19 +0900

    Add Popup and markers in map

--- src/views/SearchMap.vue
+++ src/views/SearchMap.vue
@@ -13,18 +13,25 @@
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       ></l-tile-layer>
       <l-geo-json :geojson="geojson"></l-geo-json>
+      <l-marker :lat-lng="markerLatLng"></l-marker>
+      <l-marker v-for="item in markers" :key="item.id" :lat-lng="item.latlng" @l-add="$event.target.openPopup()"> 
+          <l-popup :content="item.content"></l-popup> 
+      </l-marker> 
     </l-map>
   </div>
 </template>
 <script>
-import {LMap, LTileLayer, LGeoJson} from "@vue-leaflet/vue-leaflet";
+import {LMap, LTileLayer, LGeoJson, LMarker, LPopup} from "@vue-leaflet/vue-leaflet";
 import "leaflet/dist/leaflet.css";
+import "leaflet-geosearch/dist/geosearch.css";
 
 export default {
   components: {
     LMap,
     LTileLayer,
     LGeoJson,
+    LMarker,
+    LPopup,
   },
   data() {
     return {
@@ -32,11 +39,48 @@ export default {
       iconWidth: 25,
       iconHeight: 40,
       geojson: null,
+      markers: [],
+      markerLatLng: [51.504, -0.159]
     };
+  },
+  mounted () {
+       window.setTimeout( () => {
+        this.markers.push({
+          id: 1,
+          latlng: [47.417220, -1.222482],
+          content: 'Hi! this is my popup data'
+        });
+      }, 1000);
+       window.setTimeout( () => {
+        this.markers.push({
+          id: 2,
+          latlng: [47.417220, -1.25],
+          content: 'Another'
+        });
+      }, 2000);
   },
   async created () {
     const response = await fetch("https://france-geojson.gregoiredavid.fr/repo/regions.geojson");
     this.geojson = await response.json();
+    this.getHospitals();
   },
+  methods: {
+    getHospitals() {
+      this.$http
+        .get("/hospitals")
+        .then((response) => {
+          this.hospitals = response.data;
+          console.log(this.hospitals);
+        })
+        .catch((error) => {
+          console.log(error);
+          this.$toast.error("Erreur : " + error.response.data.detail);
+          setTimeout(this.$toast.clear, 3000);
+        });
+    }
+  }
 };
 </script>
+
+<style scoped>
+</style>
