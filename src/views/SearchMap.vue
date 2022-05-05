405ca3f5a68ae9825bda7c1ccb988de36008e414
NicolasYapobi <you@example.com>
2022-05-06 03:49:10 +0900

    Add search bar

--- src/views/SearchMap.vue
+++ src/views/SearchMap.vue
@@ -3,30 +3,37 @@
         <h1>Carte de recherche pour les hopitaux</h1>
     </div>
     <div style="height: 75vh; width: 50vw;">
-    <l-map
-      v-model="zoom"
-      v-model:zoom="zoom"
-      zoomAnimation=true
-      :center="[47, 2.25]"
-    >
-      <l-tile-layer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      ></l-tile-layer>
-      <l-geo-json :geojson="geojson"></l-geo-json>
-       <!-- <l-marker :lat-lng="markerLatLng"></l-marker> -->
-       <l-marker v-for="item in markers" :key="item.id" :lat-lng="item.latlng" @l-add="$event.target.openPopup()">
+        <input class="form-control my-0 py-1" type="text" placeholder="Rerchercher" aria-label="Search" v-model="search">
+        <div class="ui cards" style="margin: 10px">
+          <div class="card ui fluid" v-for="item in filteredHospitals" :key="item.id" style="margin: 0" v-on:click="clickHospital" :ref="item_search">
+            <div class="content">
+              <div class="header">{{ item.name }}</div>
+                <div class="meta">
+                  {{ item.city.name }} | {{ item.city.department_code}}
+                </div>
+            </div>
+        </div>
+      </div>
+      <l-map
+        v-model="zoom"
+        v-model:zoom="zoom"
+        zoomAnimation=true
+        :center="[this.latpos, this.lngpos]">
+        <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"></l-tile-layer>
+        <l-geo-json :geojson="geojson"></l-geo-json>
+        <l-marker v-for="item in markers" :key="item.id" :lat-lng="item.latlng" @l-add="$event.target.openPopup()">
           <l-popup :content="item.content"></l-popup> 
       </l-marker>
-    </l-map>
-    <div class="vignette-container">
-      <p v-for="hospital in hospitals" :key="hospital">
-          {{ hospital.name }}
-          <br>
-          Numéro de téléphone: {{ hospital.phone_number}}
-          <br>
-          Nombre de patient: {{ hospital.patients_count}}
-      </p>
-    </div>
+      </l-map>
+      <!-- <div class="vignette-container">
+        <p v-for="hospital in hospitals" :key="hospital">
+            {{ hospital.name }}
+            <br>
+            Numéro de téléphone: {{ hospital.phone_number}}
+            <br>
+            Nombre de patient: {{ hospital.patients_count}}
+        </p>
+      </div> -->
   </div>
 </template>
 <script>
@@ -47,19 +54,31 @@ export default {
       zoom: 6.4,
       iconWidth: 25,
       iconHeight: 40,
+      latpos: 47,
+      lngpos: 2.25,
       geojson: null,
       markers: [],
       markers_length: 0,
+      hospitals: [],
+      search: ""
     };
   },
   mounted () {
       
   },
+
+  computed: {
+    filteredHospitals() {
+      return this.hospitals.filter(p => {
+        return p.name.toLowerCase().indexOf(this.search.toLowerCase()) != -1;
+      });
+    }
+  },
+
   async created () {
     const response = await fetch("https://france-geojson.gregoiredavid.fr/repo/regions.geojson");
     this.geojson = await response.json();
     this.getHospitals();
-    console.log("TOTOTOTOOTO", this.markers_length);
     window.setTimeout( () => {        
         for (var i = 0; i < this.markers_length; i += 1) {
           console.log(this.hospitals[i]);
@@ -78,6 +97,17 @@ export default {
     }, 12000);
   },
   methods: {
+    clickHospital (e) {
+      for (var i = 0; i < this.markers_length; i +=1) {
+        if (e.target.textContent == this.hospitals[i].name) {
+          console.log(this.hospitals[i].latitude)
+          this.latpos = this.hospitals[i].latitude;
+          this.lngpos = this.hospitals[i].longitude;
+          this.zoom = 13;
+        }
+        
+      }
+    },
     getHospitals() {
       this.$http
         .get("/hospitals")
