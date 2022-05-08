f4d1d48b6883ac4786d5f71574ea4a82d37e0e36
OkilSaber <okil-saber.lakhdari@epitech.eu>
2022-05-09 03:20:18 +0900

    Add UI to edit a user

--- src/views/AdministratorEditUser.vue
+++ src/views/AdministratorEditUser.vue
@@ -1,28 +1,25 @@
 <template>
   <div>
-    <form class="form-control">
+    <form @submit.prevent="submitForm()" class="form-control">
       <div class="form-fields">
         <label class="label">Mail</label>
-        <input v-model="user.email" type="email" class="input mb-6" required/>
-        <div class="form-input small">
-          <label class="label">Prénom</label>
-          <input type="text" class="input mb-6"/>
-        </div>
-        <div class="form-input small">
-          <label class="label">Nom</label>
-          <input type="text" class="input mb-6"/>
-        </div>
+        <input v-model="user.email" type="email" class="input mb-6" required />
+        <label class="label">Rôle</label>
+        <select v-model="selected_role">
+          <option v-for="role in roles" :value="role.id" :key="role.id">
+            {{ role.name }}
+          </option>
+        </select>
+        <button type="submit" class="button is-info">Enregistrer</button>
       </div>
-      <button type="submit" class="button is-info">Enregistrer</button>
-      <router-link class="button is-danger ml-6" to="/administrator">Retour</router-link>
-
+      <router-link class="button is-danger ml-6" to="/administrator"
+        >Retour</router-link
+      >
     </form>
-
   </div>
 </template>
 
 <script>
-
 export default {
   name: "AdministratorEditUser",
   props: {
@@ -31,32 +28,49 @@ export default {
   data() {
     return {
       user: {},
+      roles: [],
+      selected_role: null,
     };
   },
   methods: {
     getUserByID() {
       this.$http
-          .get(`/users/${this.id}`)
-          .then((response) => {
-            this.user = response.data;
-          })
-          .catch((error) => {
-            console.log(error);
-          });
+        .get(`/users/${this.id}`)
+        .then((response) => {
+          this.user = response.data;
+          this.selected_role = response.data.role_id;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    getRoles() {
+      this.$http
+        .get("/roles")
+        .then((response) => {
+          this.roles = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
     submitForm() {
       this.$http
-          .post({})
-          .then(() => {
-            this.$router.push("/administrator");
-          })
-          .catch((error) => {
-            console.log(error);
-          });
+        .post(`/users/${this.id}`, {
+          email: this.user.email,
+          role_id: this.selected_role,
+        })
+        .then(() => {
+          this.$router.push("/administrator");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
   },
   created() {
     this.getUserByID();
+    this.getRoles();
   },
 };
 </script>
