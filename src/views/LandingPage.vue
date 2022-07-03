743a8ebef48acf3a16ad39d36d8e5dbd7025850a
irama <irama.chaouch@epitech.eu>
2022-07-04 01:02:13 +0900

    Remove Carousel from Landing page -- ref 2

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,39 +1,14 @@
 
 <template>
   <h1>Bienvenue sur la plateforme ORGANIA</h1>
-  <carousel :autoplay="4000" :wrap-around="true" :items-to-show="1.5">
-    <Slide v-for="content, index in contents" :key="content.id">
-      <div class="carousel-item">
-        <div class="carousel-content">
-          <p>{{ content.text }}</p>
-          <p>{{ index.text }}</p>
-          <router-link :to="content.url" class="carousel-link">
-            <span>En savoir plus</span>
-          </router-link>
-        </div>
-      </div>
-    </Slide>
-
-    <template #addons>
-      <navigation />
-      <pagination />
-    </template>
-  </carousel>
 </template>
 
 <script>
-// If you are using PurgeCSS, make sure to whitelist the carousel CSS classes
-import 'vue3-carousel/dist/carousel.css';
-import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
 
 export default {
   name: "landing-page",
   emits: ["login"],
   components: {
-    Carousel,
-    Slide,
-    Pagination,
-    Navigation,
   },
   data() {
     return {
@@ -49,39 +24,4 @@ export default {
 </script>
 
 <style scoped>
-.carousel-item {
-  min-height: 200px;
-  width: 100%;
-  background-color: #0f2c59;
-  color: var(--vc-clr-white);
-  font-size: 20px;
-  border-radius: 8px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.carousel-slide {
-  padding: 10px;
-}
-
-.carousel-prev,
-.carousel--next {
-  box-sizing: content-box;
-  border: 5px solid white;
-}
-
-
-.carousel-prev--in-active,
-.carousel-next--in-active {
-  display: none;
-}
-
-.carousel-content {
-  display: inline;
-}
-
-.carousel-link {
-  color: white;
-}
 </style>
