11057df071f339c35339e9875b6d39a21e2bce44
irama <irama.chaouch@epitech.eu>
2022-07-06 02:39:55 +0900

    get user mail + put information in colum

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -1,42 +1,71 @@
 <template>
     <button class="button is-link is-light is-large is-responsive is-rounded is-focused"><strong>MON
             COMPTE</strong></button><br><br>
-    <article class="message is-info">
-        <div class="message-header">
-            <p>Nom</p>
-        </div>
-        <div class="message-body">
-            <strong>CHAOUCH</strong>
-        </div>
-    </article>
-    <article class="message is-info">
-        <div class="message-header">
-            <p>Prénom</p>
-        </div>
-        <div class="message-body">
-            <strong>Irama</strong>
-        </div>
-    </article>
-    <article class="message is-info">
-        <div class="message-header">
-            <p>Numéro de téléphone</p>
-        </div>
-        <div class="message-body">
-            <strong>+33 6 41 02 70 22</strong>
-        </div>
-    </article>
-    <article class="message is-info">
-        <div class="message-header">
-            <p>Email</p>
-        </div>
-        <div class="message-body">
-            <strong>irama@irama.com</strong>
-        </div>
-    </article>
+    <div class="columns">
+        <article class="message is-info column">
+            <div class="message-header">
+                <p>Nom</p>
+            </div>
+            <div class="message-body">
+                <strong>CHAOUCH</strong>
+            </div>
+        </article>
+        <article class="message is-info column">
+            <div class="message-header">
+                <p>Prénom</p>
+            </div>
+            <div class="message-body">
+                <strong>Irama</strong>
+            </div>
+        </article>
+    </div>
+    <div class="columns">
+        <article class="message is-info column">
+            <div class="message-header">
+                <p>Numéro de téléphone</p>
+            </div>
+            <div class="message-body">
+                <strong>+33 6 41 02 70 22</strong>
+            </div>
+        </article>
+        <article class="message is-info column">
+            <div class="message-header">
+                <p>Email</p>
+            </div>
+            <div class="message-body">
+                <strong>{{ email }}</strong>
+            </div>
+        </article>
+    </div>
 </template>
 
+<script>
+
+export default {
+    name: "my-account",
+    emits: ["login"],
+    data() {
+        return {
+            email: "",
+        }
+    },
+    created() {
+        this.getMyinfo();
+    },
+    methods: {
+        getMyinfo() {
+            this.$http
+                .get("/users/me", {
+                    headers: { Authorization: `Bearer ${this.$cookies.get("token")}` },
+                })
+                .then((response) => {
+                    console.log(response);
+                    this.email = response.data.email;
+                })
+        },
+    },
+};
+</script>
+
 <style scoped>
-.block:not(:last-child) {
-    margin-bottom: 1.5rem;
-}
 </style>
