c84012487bd463ed9e6bb12f0a95460498392b5f
irama <irama.chaouch@epitech.eu>
2022-08-25 03:04:39 +0200

    modified team + add photo to the team box

--- src/views/Vitrine.vue
+++ src/views/Vitrine.vue
@@ -262,298 +262,196 @@
           </li>
         </ul>
       </div>
-      <h1 class="is-size-2 has-text-centered titre-about" id="team">L'équipe</h1>
-
+      <h1 class="is-size-2 has-text-centered title" id="team">L'équipe</h1>
+      <br>
       <div class="resized-container">
 
-        <div class="columns is-2">
-          <div class="column">
-            <div class="team-box">
-              <div class="team-left">
-                <div class="team-image">D</div>
-                <div class="team-name">David FARJON</div>
-                <a class="team-button linkedin" href="https://www.linkedin.com/in/david-farjon-863b471b5/">
-                  <div class="fab fa-linkedin"></div>
-                  Linkedin
-                </a>
-                <a class="team-button github" href="https://github.com/davidfarjonepitech">
-                  <div class="fab fa-github"></div>
-                  Github
-                </a>
-                <a class="team-button instagram">
-                  <div class="fab fa-instagram"></div>
-                  Instagram
-                </a>
-                <a class="team-button twitter">
-                  <div class="fab fa-twitter"></div>
-                  Twitter
-                </a>
+        <div class="card card-style">
+          <div class="card-content">
+            <div class="media">
+              <div class="media-left">
+                <figure class="image is-128x128">
+                  <img class="is-rounded" src="./photo-vitrine/david.farjon.jpg" alt="Placeholder image">
+                </figure>
               </div>
-              <div class="team-middle"></div>
-              <div class="team-right">
-                <div class="team-text">
-                  Passionné de système d'exploitation, je rêve de pouvoir un jour créer
-                  mon propre système d'exploitation depuis mon plus jeune âge.<br />
-                  La realité augmenté et l'inteligence artificielle m'attire aussi.<br />
-                  Travaillant principalement sur le front du Organ-IA.<br />
-                  Epitech 4ème année.<br />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="column">
-            <div class="team-box">
-              <div class="team-left">
-                <div class="team-image">W</div>
-                <div class="team-name">William WINQWIST</div>
-                <a class="team-button linkedin" href="https://www.linkedin.com/in/william-winqwist-a38325152/">
-                  <div class="fab fa-linkedin"></div>
-                  Linkedin
-                </a>
-                <a class="team-button github" href="https://github.com/WilliamWnq">
-                  <div class="fab fa-github"></div>
-                  Github
-                </a>
-                <a class="team-button instagram">
-                  <div class="fab fa-instagram"></div>
-                  Instagram
-                </a>
-                <a class="team-button twitter">
-                  <div class="fab fa-twitter"></div>
-                  Twitter
-                </a>
-              </div>
-              <div class="team-middle"></div>
-              <div class="team-right">
-                <div class="team-text"></div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="columns is-2">
-          <div class="column">
-            <div class="team-box">
-              <div class="team-left">
-                <div class="team-image">D</div>
-                <div class="team-name">Damien SAVATIER</div>
-                <a class="team-button linkedin" href="https://www.linkedin.com/in/tina-otoge/">
-                  <div class="fab fa-linkedin"></div>
-                  Linkedin
-                </a>
-                <a class="team-button github" href="https://github.com/Tina-otoge">
-                  <div class="fab fa-github"></div>
-                  Github
-                </a>
-                <a class="team-button instagram">
-                  <div class="fab fa-instagram"></div>
-                  Instagram
-                </a>
-                <a class="team-button twitter">
-                  <div class="fab fa-twitter"></div>
-                  Twitter
-                </a>
-              </div>
-              <div class="team-middle"></div>
-              <div class="team-right">
-                <div class="team-text"></div>
-              </div>
-            </div>
-          </div>
-          <div class="column">
-            <div class="team-box">
-              <div class="team-left">
-                <div class="team-image">F</div>
-                <div class="team-name">Felix BURDOT</div>
-                <a class="team-button linkedin" href="https://www.linkedin.com/in/f%C3%A9lix-burdot-8b0686159/">
-                  <div class="fab fa-linkedin"></div>
-                  Linkedin
-                </a>
-                <a class="team-button github" href="https://github.com/flxui">
-                  <div class="fab fa-github"></div>
-                  Github
-                </a>
-                <a class="team-button instagram">
-                  <div class="fab fa-instagram"></div>
-                  Instagram
-                </a>
-                <a class="team-button twitter">
-                  <div class="fab fa-twitter"></div>
-                  Twitter
-                </a>
-              </div>
-              <div class="team-middle"></div>
-              <div class="team-right">
-                <div class="team-text">
-                  Je suis Félix Burdot, jeune développeur à la recherche d'un domaine dans lequel me lancer
-                  professionnellement, je cherche à accumuler le plus d'expérience et de connaissances possible via mes
-                  expériences professionnelles et projets personnels.
-
+              <div class="media-content">
+                <div class="media media-style">
+                  <div class="media-left">
+                    <p class="title is-4">David Farjon</p>
+                    <p class="subtitle is-6">Développeur FrontEnd</p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div class="columns is-2">
-          <div class="column">
-            <div class="team-box">
-              <div class="team-left">
-                <div class="team-image">G</div>
-                <div class="team-name">Guillaume BRAUX</div>
-                <a class="team-button linkedin" href="https://www.linkedin.com/in/guillaume-braux/">
-                  <div class="fab fa-linkedin"></div>
-                  Linkedin
-                </a>
-                <a class="team-button github" href="https://github.com/GuillaumeBraux">
-                  <div class="fab fa-github"></div>
-                  Github
-                </a>
-                <a class="team-button instagram">
-                  <div class="fab fa-instagram"></div>
-                  Instagram
-                </a>
-                <a class="team-button twitter">
-                  <div class="fab fa-twitter"></div>
-                  Twitter
-                </a>
+        <br>
+        <div class="card card-style">
+          <div class="card-content">
+            <div class="media">
+              <div class="media-left">
+                <figure class="image is-128x128">
+                  <img class="is-rounded" src="./photo-vitrine/william.jpeg" alt="Placeholder image">
+                </figure>
               </div>
-              <div class="team-middle"></div>
-              <div class="team-right">
-                <div class="team-text"></div>
-              </div>
-            </div>
-          </div>
-          <div class="column">
-            <div class="team-box">
-              <div class="team-left">
-                <div class="team-image">N</div>
-                <div class="team-name">Nicolas YAPOBI</div>
-                <a class="team-button linkedin" href="https://www.linkedin.com/in/nicolasyapobi1445a158/">
-                  <div class="fab fa-linkedin"></div>
-                  Linkedin
-                </a>
-                <a class="team-button github" href="https://github.com/NicolasYapobi">
-                  <div class="fab fa-github"></div>
-                  Github
-                </a>
-                <a class="team-button instagram">
-                  <div class="fab fa-instagram"></div>
-                  Instagram
-                </a>
-                <a class="team-button twitter">
-                  <div class="fab fa-twitter"></div>
-                  Twitter
-                </a>
-              </div>
-              <div class="team-middle"></div>
-              <div class="team-right">
-                <div class="team-text">
-                  Je m'appelle YAPOBI Nicolas, développeur passionné en robotique et tout ce qui n'est pas complexe.
-                  Effectuant mon année en Corée du Sud, j'ai pour but d'y vivre et de travailler au sein de Samsung
+              <div class="media-content">
+                <div class="media media-style">
+                  <div class="media-left">
+                    <p class="title is-4">William Winqwist</p>
+                    <p class="subtitle is-6">Développeur </p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div class="columns is-2">
-          <div class="column">
-            <div class="team-box">
-              <div class="team-left">
-                <div class="team-image">V</div>
-                <div class="team-name">Victor HAMON</div>
-                <a class="team-button linkedin" href="https://www.linkedin.com/in/victor-hamon-0478b817a/">
-                  <div class="fab fa-linkedin"></div>
-                  Linkedin
-                </a>
-                <a class="team-button github" href="https://github.com/Victor-Hamon">
-                  <div class="fab fa-github"></div>
-                  Github
-                </a>
-                <a class="team-button instagram">
-                  <div class="fab fa-instagram"></div>
-                  Instagram
-                </a>
-                <a class="team-button twitter">
-                  <div class="fab fa-twitter"></div>
-                  Twitter
-                </a>
+        <br>
+        <div class="card card-style">
+          <div class="card-content">
+            <div class="media">
+              <div class="media-left">
+                <figure class="image is-128x128">
+                  <img class="is-rounded" src="" alt="Placeholder image">
+                </figure>
               </div>
-              <div class="team-middle"></div>
-              <div class="team-right">
-                <div class="team-text">Depuis que je suis jeune je me suis intéressé à l'informatique,
-                  j'adore construire différents projets via mes connaissances tout en apprenant et en perfectionnant
-                  d'autres compétences que j'acquiers au cours de ces "aventures", également passionné de biologie
-                  moléculaire, je souhaite m'orienter dans un travail me permettant d'améliorer la
-                  compréhension du fonctionnement des protéines à l'aide de l'informatique</div>
-              </div>
-            </div>
-          </div>
-          <div class="column">
-            <div class="team-box">
-              <div class="team-left">
-                <div class="team-image">O</div>
-                <div class="team-name">Okil Saber LAKHDARI</div>
-
-                <a class="team-button github" href="https://github.com/OkilSaber">
-                  <div class="fab fa-github"></div>
-                  Github
-                </a>
-                <a class="team-button linkedin-saber" href="https://www.linkedin.com/in/okil-saber-lakhdari-30ab02176/">
-                  <div class="fab fa-linkedin"></div>
-                  Linkedin
-                </a>
-                <a class="team-button twitter">
-                  <div class="fab fa-twitter"></div>
-                  Twitter
-                </a>
-              </div>
-              <div class="team-middle"></div>
-              <div class="team-right">
-                <div class="team-text">
-                  Je m'appelle LAKHDARI Okil Saber, jeune développeur, <br>je suis passionné par le développement mobile
-                  Flutter
-                  <br>
-                  et aimerait en faire ma carrière. <br>Je suis un énorme fan de sport automobile, de la F1 au Gr.3,<br>
-                  je porte
-                  Mercedes et Porsche dans mon cœur.<br> Je suis aussi un amateur de sim-racing.
-
+              <div class="media-content">
+                <div class="media media-style">
+                  <div class="media-left">
+                    <p class="title is-4">Damien Savatier</p>
+                    <p class="subtitle is-6">Développeur </p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div class="columns is-2">
-          <div class="column">
-            <div class="team-box">
-              <div class="team-left">
-                <div class="team-image">I</div>
-                <div class="team-name">Irama Chaouch</div>
-                <a class="team-button linkedin" href="https://www.linkedin.com/in/irama-chaouch-95268517a/">
-                  <div class="fab fa-linkedin"></div>
-                  Linkedin
-                </a>
-                <a class="team-button github" href="https://github.com/iramach">
-                  <div class="fab fa-github"></div>
-                  Github
-                </a>
-                <a class="team-button instagram">
-                  <div class="fab fa-instagram"></div>
-                  Instagram
-                </a>
-                <a class="team-button twitter">
-                  <div class="fab fa-twitter"></div>
-                  Twitter
-                </a>
+        <br>
+        <div class="card card-style">
+          <div class="card-content">
+            <div class="media">
+              <div class="media-left">
+                <figure class="image is-128x128">
+                  <img class="is-rounded" src="./photo-vitrine/felix.jpg" alt="Placeholder image">
+                </figure>
               </div>
-              <div class="team-middle"></div>
-              <div class="team-right">
-                <div class="team-text"></div>
+              <div class="media-content">
+                <div class="media media-style">
+                  <div class="media-left">
+                    <p class="title is-4">Felix Burdot</p>
+                    <p class="subtitle is-6">Développeur </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-
+        <br>
+        <div class="card card-style">
+          <div class="card-content">
+            <div class="media">
+              <div class="media-left">
+                <figure class="image is-128x128">
+                  <img class="is-rounded" src="./photo-vitrine/guillaume.jpg" alt="Placeholder image">
+                </figure>
+              </div>
+              <div class="media-content">
+                <div class="media media-style">
+                  <div class="media-left">
+                    <p class="title is-4">Guillaume Braux</p>
+                    <p class="subtitle is-6">Développeur </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <br>
+          </div>
+        </div>
+        <br>
+        <div class="card card-style">
+          <div class="card-content">
+            <div class="media">
+              <div class="media-left">
+                <figure class="image is-128x128">
+                  <img class="is-rounded" src="./photo-vitrine/nicolas.jpg" alt="Placeholder image">
+                </figure>
+              </div>
+              <div class="media-content">
+                <div class="media media-style">
+                  <div class="media-left">
+                    <p class="title is-4">Nicolas Yapobi</p>
+                    <p class="subtitle is-6">Développeur </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br>
+        <div class="card card-style">
+          <div class="card-content">
+            <div class="media">
+              <div class="media-left">
+                <figure class="image is-128x128">
+                  <img class="is-rounded" src="./photo-vitrine/victor.jpg" alt="Placeholder image">
+                </figure>
+              </div>
+              <div class="media-content">
+                <div class="media media-style">
+                  <div class="media-left">
+                    <p class="title is-4">Victor Hamon</p>
+                    <p class="subtitle is-6">Développeur </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br>
+        <div class="card card-style">
+          <div class="card-content">
+            <div class="media">
+              <div class="media-left">
+                <figure class="image is-128x128">
+                  <img class="is-rounded" src="./photo-vitrine/saber.jpg" alt="Placeholder image">
+                </figure>
+              </div>
+              <div class="media-content">
+                <div class="media media-style">
+                  <div class="media-left">
+                    <p class="title is-4">Okil Saber Lakhdari</p>
+                    <p class="subtitle is-6">Développeur </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br>
+        <div class="card card-style">
+          <div class="card-content">
+            <div class="media">
+              <div class="media-left">
+                <figure class="image is-128x128">
+                  <img class="is-rounded" src="./photo-vitrine/irama.jpg" alt="Placeholder image">
+                </figure>
+              </div>
+              <div class="media-content">
+                <div class="media media-style">
+                  <div class="media-left">
+                    <p class="title is-4">Irama Chaouch</p>
+                    <p class="subtitle is-6">Développeuse FrontEnd </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
+      <div class="resized-container">
+        <div class="columns is-2">
+        </div>
+      </div>
     </div>
 
     <br>
@@ -846,5 +744,14 @@ li.tl-li:before {
 .titre-about {
   background-color: var(--dark-blue);
   color: white;
+}
+
+.card-style {
+  width: 30em;
+  height: 14em;
+}
+
+.media-style {
+  text-align: center;
 }
 </style>
