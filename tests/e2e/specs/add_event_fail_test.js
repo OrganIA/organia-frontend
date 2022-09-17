8866375971c721bb713448ae0f7a683f5c0a2717
NicolasYapobi <you@example.com>
2022-09-17 13:56:49 +0200

    Update event test

--- tests/e2e/specs/add_event_fail_test.js
+++ tests/e2e/specs/add_event_fail_test.js
@@ -1,11 +1,14 @@
 /* eslint-disable no-undef */
 describe('Add event fail', () => {
   it('Tries to open modal in the event panel should fail', () => {
-    cy.visit('http://organia.francecentral.cloudapp.azure.com/')
+    cy.visit('http://localhost:8081/login')
 
     cy.get('.cypress-to-register').click()
+    cy.url().should('eq', 'http://localhost:8081/register')
+
 
     cy.get('.cypress-to-login').click()
+    cy.url().should('eq', 'http://localhost:8081/login')
 
     cy.get('.cypress-email')
       .type('saber@saber.com')
@@ -17,17 +20,17 @@ describe('Add event fail', () => {
 
     cy.get('.cypress-login').click()
 
-    cy.url().should('eq', 'http://organia.francecentral.cloudapp.azure.com/')
+    cy.url().should('eq', 'http://localhost:8081/')
 
     cy.getCookie("token").should('not.be.null')
 
     cy.get('.cypress-to-eventlist').click();
 
-    cy.url().should('eq', 'http://organia.francecentral.cloudapp.azure.com/eventlist')
+    cy.url().should('eq', 'http://localhost:8081/eventlist')
 
     cy.get('.cypress-to-add').click();
 
-    cy.url().should('eq', 'http://organia.francecentral.cloudapp.azure.com/eventlist/add')
+    cy.url().should('eq', 'http://localhost:8081/eventlist/add')
 
     cy.get('.cypress-datetime')
       .type('2022-06-01T14:44')
@@ -35,6 +38,6 @@ describe('Add event fail', () => {
 
     cy.get('.cypress-add').click();
 
-    cy.url().should('eq', 'http://organia.francecentral.cloudapp.azure.com/eventlist/add')
+    cy.url().should('eq', 'http://localhost:8081/eventlist/add')
   })
 })
--- tests/e2e/specs/add_event_succeed_test.js
+++ tests/e2e/specs/add_event_succeed_test.js
@@ -1,11 +1,13 @@
 /* eslint-disable no-undef */
 describe('Add Event', () => {
   it('Tries to open modal in the event panel should succeed', () => {
-    cy.visit('http://organia.francecentral.cloudapp.azure.com/')
+    cy.visit('http://localhost:8081/login')
 
     cy.get('.cypress-to-register').click()
+    cy.url().should('eq', 'http://localhost:8081/register')
 
     cy.get('.cypress-to-login').click()
+    cy.url().should('eq', 'http://localhost:8081/login')
 
     cy.get('.cypress-email')
       .type('saber@saber.com')
@@ -17,17 +19,17 @@ describe('Add Event', () => {
 
     cy.get('.cypress-login').click()
 
-    cy.url().should('eq', 'http://organia.francecentral.cloudapp.azure.com/')
+    cy.url().should('eq', 'http://localhost:8081/')
 
     cy.getCookie("token").should('not.be.null')
 
     cy.get('.cypress-to-eventlist').click();
 
-    cy.url().should('eq', 'http://organia.francecentral.cloudapp.azure.com/eventlist')
+    cy.url().should('eq', 'http://localhost:8081/eventlist')
 
     cy.get('.cypress-to-add').click();
 
-    cy.url().should('eq', 'http://organia.francecentral.cloudapp.azure.com/eventlist/add')
+    cy.url().should('eq', 'http://localhost:8081/eventlist/add')
 
     cy.get('.cypress-datetime')
       .type('2022-01-17T14:44')
@@ -39,6 +41,6 @@ describe('Add Event', () => {
 
     cy.get('.cypress-add').click();
 
-    cy.url().should('eq', 'http://organia.francecentral.cloudapp.azure.com/eventlist')
+    cy.url().should('eq', 'http://localhost:8081/eventlist/add')
   })
 })
