68c3cc9bf5fc8acfeb826029f6f296d0b233a883
NicolasYapobi <you@example.com>
2022-09-17 13:36:36 +0200

    Update logs test

--- tests/e2e/specs/check_roles_pannel.js
+++ tests/e2e/specs/check_roles_pannel.js
@@ -2,11 +2,13 @@
 
 describe('Check logs page', () => {
   it('Tries to register should succeed', () => {
-    cy.visit('http://organia.francecentral.cloudapp.azure.com/')
+    cy.visit('http://localhost:8081/login')
 
     cy.get('.cypress-to-register').click()
+    cy.url().should('eq', 'http://localhost:8081/register')
 
     cy.get('.cypress-to-login').click()
+    cy.url().should('eq', 'http://localhost:8081/login')
 
     cy.get('.cypress-email')
       .type('saber@saber.com')
@@ -18,7 +20,7 @@ describe('Check logs page', () => {
 
     cy.get('.cypress-login').click()
 
-    cy.url().should('eq', 'http://organia.francecentral.cloudapp.azure.com/')
+    cy.url().should('eq', 'http://localhost:8081/')
 
     cy.getCookie("token").should('not.be.null')
     cy.get('.cypress-to-roles').click()
