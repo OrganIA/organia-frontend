2734b3033a32a94a6c4e4ff0ecc80b0e1bb90e76
Irama Chaouch <irama@macbook-pro-de-irama.home>
2022-11-12 18:24:32 +0100

    add landing folder with test for landing page, 1st connect and acced to landing page

---
+++ cypress/e2e/landing/landing-page-check.cy.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-undef */
+describe('Check Landing page element', () => {
+    it('Tries to open modal in the receiver panel should succeed', () => {
+        cy.visit(Cypress.config().baseUrl)
+
+        cy.get('.cypress-to-login').click()
+    
+        cy.get('.cypress-email')
+          .type('test@irama.com')
+          .should('have.value', 'test@irama.com')
+    
+        cy.get('.cypress-password')
+          .type('irama')
+          .should('have.value', 'irama')
+    
+        cy.get('.cypress-submit-login').click()
+        cy.intercept('POST', '**/auth').as('login')
+        //cy.wait('@login', { timeout: 10000 })
+    
+        cy.url().should('eq', 'http://localhost:8080/landing')
+        cy.getCookie("token").should('not.be.null')
+    })
+})
